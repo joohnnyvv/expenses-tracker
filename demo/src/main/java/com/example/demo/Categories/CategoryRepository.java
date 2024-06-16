a57e4b2adb167920f639a5f37a0091513950a352
Jan <janekrembikowski@gmail.com>
2024-06-17 00:48:59 +0200

    optimize imports

--- demo/src/main/java/com/example/demo/Categories/CategoryRepository.java
+++ demo/src/main/java/com/example/demo/Categories/CategoryRepository.java
@@ -1,10 +1,7 @@
 package com.example.demo.Categories;
 
-import com.example.demo.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
